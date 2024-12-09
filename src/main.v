779e86e7b5b3704534ffc4eb13b734cf3ed063bf
Jeremy@JYL <jeremylow823@outlook.com>
2024-12-09 21:45:34 +0800

    Fixed error handling in main.v

--- src/main.v
+++ src/main.v
@@ -4,7 +4,7 @@ fn main() {
 	interp := C.Tcl_CreateInterp()
 	C.Tcl_Init(interp)
 	C.Tk_Init(interp)
-	if C.Tcl_Eval(interp, c'wm title . hello; pack [button .h -text "Hello, World!" -command exit]') != 0 {
+	if C.Tcl_Eval(interp, c'wm title . hello; pack [button .h -text "Hello, World!" -command exit]') == C.TCL_ERROR {
 		panic(unsafe { cstring_to_vstring(C.Tcl_GetStringResult(interp)) })
 	}
 	C.Tk_MainLoop()
