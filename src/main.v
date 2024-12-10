bffebeb78d3d554097457fd55d4d3b23e1ea6166
Jeremy@JYL <jeremylow823@outlook.com>
2024-12-10 13:42:59 +0800

    Added CreateObjCommand. Credit: Thanks for spytheman (On discord) helping me out

--- src/main.v
+++ src/main.v
@@ -1,10 +1,16 @@
 module main
 
+fn greet(client_data voidptr, interp &C.Tcl_Interp, objc int, objv &&C.Tcl_Obj) int {
+	println('Hello')
+	return C.TCL_OK
+}
+
 fn main() {
 	interp := C.Tcl_CreateInterp()
 	C.Tcl_Init(interp)
 	C.Tk_Init(interp)
-	if C.Tcl_Eval(interp, c'wm title . hello; pack [button .h -text "Hello, World!" -command exit]') == C.TCL_ERROR {
+	C.Tcl_CreateObjCommand(interp, c'greet', greet, unsafe { nil }, unsafe { nil })
+	if C.Tcl_Eval(interp, c'wm title . hello; pack [button .h -text "Hello, World!" -command greet]') == C.TCL_ERROR {
 		panic(unsafe { cstring_to_vstring(C.Tcl_GetStringResult(interp)) })
 	}
 	C.Tk_MainLoop()
--- src/tcl.c.v
+++ src/tcl.c.v
@@ -1,20 +1,16 @@
 module tcl
 
 #flag -ltcl
+#flag -Wno-error=incompatible-pointer-types
 #include <tcl.h>
 
 pub struct C.Tcl_Interp {}
 
-pub struct C.ClientData {}
-
 pub struct C.Tcl_Obj {}
-
-pub struct C.Tcl_Command {}
-
-pub struct C.Tcl_CmdInfo {}
 
 fn C.Tcl_CreateInterp() &C.Tcl_Interp
 fn C.Tcl_Init(&C.Tcl_Interp) int
 fn C.Tcl_Eval(&C.Tcl_Interp, &char) int
 fn C.Tcl_GetStringResult(&C.Tcl_Interp) &char
 fn C.Tcl_DeleteInterp(&C.Tcl_Interp)
+fn C.Tcl_CreateObjCommand(&C.Tcl_Interp, &char, voidptr, voidptr, voidptr) voidptr
